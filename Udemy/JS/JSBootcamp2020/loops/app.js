7f131db63e9ea4ebc1689d74afca7f5b85c9a1a6
Tom Fraser <tom.fraser@sykescottages.co.uk>
2020-11-03 17:16:40 +0000

    loops

---
+++ Udemy/JS/JSBootcamp2020/loops/app.js
@@ -0,0 +1,11 @@
+// For Loops
+
+
+// In a for loop... the first argument defines the starting point
+// Second argument is the condition, how many time should the loop run
+// Third argument is what change to the value each time the loop is ran.
+
+//   start point        stop at     add 1 to i each time    
+for(let i = 1;          i <= 10;    i++ ) {
+    console.log(i);
+}
