fb066d3f8f6b16f558459cc8a8e7759994ddd1ee
fray56 <tom@apollodev.co.uk>
2020-11-03 21:23:16 +0000

    more loops

--- Udemy/JS/JSBootcamp2020/loops/app.js
+++ Udemy/JS/JSBootcamp2020/loops/app.js
@@ -9,3 +9,65 @@
 for(let i = 1;          i <= 10;    i++ ) {
     console.log(i);
 }
+
+
+// For loops in an Array
+
+const examResults = [
+    12,
+    45,
+    34,
+    78,
+    43,
+    23,
+    56,
+    23
+];
+
+for (let i = 0; i < examResults.length; i++) {
+    console.log(i, examResults[i]);
+}
+
+// For loop through an object
+
+const myStudents = [
+    {
+        firstName: 'Tom',
+        grade: 66
+    },
+    {
+        firstName: 'Amy',
+        grade: 78
+    },
+    {
+        firstName: 'Jo',
+        grade: 78
+    }
+];
+
+for (let i = 0; i < myStudents.length; i++) {
+    let student = myStudents[i];
+    console.log(`${student.firstName} got a grade of ${student.grade}`);
+}
+
+
+// Nested for loops
+
+const gameBoard = [
+    [54, 32, 56, 19],
+    [32, 43, 23, 54],
+    [92, 32, 34, 12],
+    [99, 12, 34, 76]
+];
+
+totalScore = 0;
+
+for (i = 0; i < gameBoard.length; i++) {
+    const row = gameBoard[i];
+
+    for (j = 0; j < row.length; j++) {
+        console.log(totalScore += row[j]);
+    }
+}
+
+
