f259fbd32a472b22b081f3779c72e8ee857647ad
fray56 <tom@apollodev.co.uk>
2020-11-26 10:17:26 +0000

    async cont

---
+++ Udemy/JS/JSBootcamp2020/Async-Await/app.js
@@ -0,0 +1,13 @@
+// Async keyword
+//
+// the async keyword is written infron of the function call.
+// this will make the funtion behave differently.
+// it will make the function return a promise
+//
+// 
+async function hello() {
+    return 'Hello there'
+};
+// the hello() function will return a function that will be resolved
+// and the value of the promise will be 'hello there'
+
