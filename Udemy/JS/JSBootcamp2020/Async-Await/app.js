617cb28c8b4a1702dc25c90acf5007e694c593b6
Tom Fraser <fray@MacBook-Pro.local>
2020-11-27 17:32:33 +0000

    prototypes

--- Udemy/JS/JSBootcamp2020/Async-Await/app.js
+++ Udemy/JS/JSBootcamp2020/Async-Await/app.js
@@ -4,6 +4,8 @@
 // this will make the funtion behave differently.
 // it will make the function return a promise
 //
+// its just nice shorter syntax than manually writing a new promise with resolve and reject methods.
+//
 // 
 async function hello() {
     return 'Hello there'
@@ -11,3 +13,86 @@ async function hello() {
 // the hello() function will return a function that will be resolved
 // and the value of the promise will be 'hello there'
 
+// we can chain .then methods onto async functions.
+// we add a argument to the the .then so we can have access 
+// to the return value in the promise call.
+
+hello().then((data) => {
+    console.log(`This was called using async functions and the data says: ${data}`);
+})
+
+
+// when using async the promise will always be resolved.
+// if we want the promise to be rejected we can throw and new exception.
+// we can do this by useing the 'throw' keyword
+async function add(x,y) {
+    if(typeof x !== 'number' || typeof y !== 'number') {
+        throw 'X and Y must be a number!';
+    }
+    return x + y;
+}
+
+// we can chain on .then and .catch methods as well
+// as long as we always return a promise.
+
+add('e', 't')
+.then(val => {
+    console.log(`This is return with ${val}`);
+})
+.catch(err => {
+    // we have access to the throw exception we made in the function call.
+    console.log(`this has failed because ${err}`);
+})
+
+
+
+//
+//
+// The Await keyword
+// can only be used inside a async function
+// when we make an async function call.
+// we can use the await keyword to make the any code written after the call not run until the promise is resloved 
+// we dont have to use .then method, 
+// save the await return value to a variable so we can have access to the data 
+
+// call an async function
+async function getPeople() {
+    // use the await keyword
+    // this makes the code wait until the api call is resolved
+    // we save it to a vairiable so we can access the data
+    const res = await axios.get('https://swapi.dev/api/people/');
+    //we can use the data straight away becasue the await keyword wont let this log run until the promise has been resolved
+    console.log(res.data);
+}
+
+getPeople();
+
+//
+// error handling
+//
+// there are two ways we can catch erros when using await
+
+async function getPlanets() {
+    const res = await axios.get('https://swapi.dev/api/planets/');
+    console.log(res.data);
+}
+// this approach is good for a catch all if there is multiple layers of .thens and functions
+//this will catch any erros.
+getPlanets().catch(err => {
+    console.log(`This did not work ${err}`);
+})
+
+
+// This is a specific way to catch any erros that are just from this function
+// good if its just calling one place and if it doesnt work it throws an error
+// wont look outside of this function
+async function getMorePlanets() {
+    try{
+        const res = await axios.get('https://swapi.dev/api/people/thiswontwork');
+        console.log(res.data);
+    }
+    catch(e) {
+        console.log(`this is an ${e}`);
+    }
+}
+getMorePlanets();
---
+++ Udemy/JS/JSBootcamp2020/Protypes-Classes/app.js
@@ -0,0 +1,150 @@
+// Prototypes 
+//
+// They are a template objects that is assigned to all javascript elements thats has all the built in methods stored.
+//
+// If we create a new array.
+//
+// const arr = [1,2,3,4,5];
+//
+// if we look at arr in the console and use the .length method.
+//
+// arr.length
+//
+// arr doesnt have its own version of .length, 
+// it has the __proto__ method which has all the methods that are built in and available to us.
+//
+// its a way of storing all the methods easily
+//
+// proto is a reference to the other built in methods that we can access.
+
+// we can add to the prototypes,
+// this will mean anything we add to the prototype will be available to that element
+//
+// we have added the 'grumpy' method to the String prototype
+// so anytime we create a new string or have a string
+// we can use the grumpoy method on it without having to create one for each string.
+String.prototype.grumpy = () => {
+    alert('Go Away!!');
+}
+// if we create a new string in the console and call the grumpy method... we should get the alert!
+
+//
+//Not a great idea to use this on projects because exiting
+
+
+// Class and OOP
+//
+// Object Orianted Programming is the idea of organising your code into structured objects
+// having blueprints of code that enables you to reuse code many times while only writing it once.
+//
+//
+// factory functions
+//
+// factory functions are functions that act like a factory to build a javascript object.
+// we can then add methods to that object so we can use them throughtout the app.
+//
+// has shortcomings and will run into problems using this way
+// not very organised and can get messy if making lots of methods
+
+
+// we make a function that will be our factory
+function makeColor(r,g,b) {
+    // create a variable that will be the object our factory will create and store the results inside
+    const color = {};
+    // assign the values passed in when we call the makeColor function
+    // these will set the object keys and the value to what we pass in to the function
+    color.r  = r;
+    color.g = g;
+    color.b = b;
+
+    // we can create a method on the object that we can call.
+   color.rgb = function() {
+       // destructor r,g,b from the this key word so we can use it without typing this.
+       const {r,g,b} = this;
+       // the values of r,g,b are scoped to the makeColor function so we can access them
+       // this returns a string that is a rgb value we can use
+       return `rgb(${r}, ${g}, ${b})`;
+   }
+
+
+    // we return the object so we can use it else where
+    return color;
+}
+//
+// this is a commomnly used way
+// better way is to use a constructor method approach
+// when using a factory function is creates a seperatre method for each color object we create.
+// its not stored on the prototype
+//
+// constructor functions adds the method to the prototype so every object we make has access to the methods on the prototype.
+// much better approach to organising code.
+
+// we create a function like above but we call the function by using the 'new' keyword.
+// this tells javascript to create a new object that we have access to
+// the 'this' keyword is automatically scoped to the new object behind the scenes
+// 
+
+function newColor(r,g,b) {
+    // we can use this because this function gets called with the 'new' keyword so 'this' is scoped to the new object and not the global window.
+    this.r = r;
+    this.g = g;
+    this.b = b;
+
+    // we can create a method
+    // using this 
+    // becuase this is referencing the object we are inside
+    //
+    // this method is still NOT added to the prototype
+    this.rgb = function () {
+        const {r,g,b} = this;
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+    
+}
+// to add the method to the prototype, we can create the method outside of the newColor function
+// not the best way. Can get messy!
+// We can still use the this keyword here
+// Have to use '.prototype.' when creating the method.
+// This is a more efficient way to create objects with methods on them.... but not the best!
+// 
+// newColor.prototype.rgb = function() {
+//     const {r,g,b} = this;
+//         return `rgb(${r}, ${g}, ${b})`;
+// }
+
+const nextColor = new newColor(32,100,88);
+
+
+
+// Classes
+//
+// This is a much better way to creating objects and organising code
+//
+// Same as above but better syntax and easier and quicker to write.
+//
+// Dont have to add methods to the protoype manually... it does it for you
+//
+
+//we use the class key word to initialise the class object
+// the name of the class is always with a capital letter.
+
+class Color {
+    // we always have to add the 'constructor()' function
+    // this function is ran everytime a new Color is crerated.
+    // the constructor function is like the factory function above
+    // it builds the Color object for us.
+    constructor(r,g,b, name) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+    }
+
+    rgb() {
+        const {r,g,b,name} = this;
+        console.log(`this is ${name}`);
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+}
+// when calling a new Color we have to use the 'new' keyword
+const c1 = new Color(34,46,77, 'my colour');
